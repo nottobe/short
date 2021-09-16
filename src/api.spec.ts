bd79324c31313401386a474d01e96b1ba7cdca72
Tobias Mahnke <tobmah@users.noreply.github.com>
2021-09-16 11:05:12 +0200

    implement POST & GET

--- src/api.spec.ts
+++ src/api.spec.ts
@@ -2,10 +2,22 @@ import api from "./api";
 import * as request from "supertest";
 
 describe("POST /", () => {
+    it("requires url parameter", async () => {
+        const response = await request(api).post('/')
+        .set('Content-Type', 'application/json')
+        .send({});
+        expect(response.statusCode).toEqual(400);
+    });
     it("generates short url", async () => {
-        const response = await request(api).post('/');
-        expect(response.text).toBe(JSON.stringify({ shortUrl: '' }));
+        const response = await request(api).post('/')
+        .set('Content-Type', 'application/json')
+        .send({
+            url: "https://www.google.com"
+        });
         expect(response.statusCode).toEqual(200);
+        const body = JSON.parse(response.text);
+
+        expect(body?.shortUrl).toMatch(/http:\/\/tier.app\//);
     });
 });
 
@@ -13,5 +25,24 @@ describe("GET /", () => {
     it("returns 404 unknown", async () => {
         const response = await request(api).get('/unknown');
         expect(response.statusCode).toEqual(404);
-    })
+    });
+
+    it("returns 301 for created id", async () => {
+        const createResponse = await request(api).post('/')
+        .set('Content-Type', 'application/json')
+        .send({
+            url: "https://www.google.com"
+        });
+
+        expect(createResponse.statusCode).toEqual(200);
+        const body = JSON.parse(createResponse.text);
+
+        const shortUrl = body?.shortUrl.match(/http:\/\/tier.app\/(.*)/);
+        expect(shortUrl.length).toBe(2);
+        const id = shortUrl[1];
+
+        const response = await request(api).get(`/${id}`);
+        expect(response.statusCode).toEqual(301);
+        expect(response.header?.location).toBe('https://www.google.com');
+    });
 });
--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,31 @@
 import * as express from "express";
+import { nanoid } from 'nanoid';
+
+const BASE_URL = 'tier.app';
+
+const SHORT_URLS = new Map();
 
 const app = express();
 
-app.post("/", (_req, res) => {
-    res.json({ shortUrl: ''});
+app.use(express.json());
+
+app.post("/", (req, res) => {
+    if(req.body?.url) {
+        const id = nanoid();
+        SHORT_URLS.set(id, req.body?.url);
+        res.json({ shortUrl: `${req.protocol}://${BASE_URL}/${id}` });
+    } else {
+        res.sendStatus(400);
+    }
 });
 
-app.get("/", (_req, res) => {
-    res.status(404);
+app.get("/*", (req, res) => {
+    const id = req.path.replace('/', '');
+
+    if (SHORT_URLS.has(id)) {
+        return res.redirect(301 , SHORT_URLS.get(id));
+    }
+    res.sendStatus(404);
 });
 
 export default app;
